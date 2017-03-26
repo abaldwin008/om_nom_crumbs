e158cfba9dd6fd404e462e762da23c967d2a9412
Amy Baldwin <abaldwin008@gmail.com>
2017-03-26 22:12:19 +0000

    Update home and about pages with placeholder information

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Om Nom Crumbs"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Om Nom Crumbs"
   end
-
+  
+  
 end
