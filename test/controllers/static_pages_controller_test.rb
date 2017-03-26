2e248aaea63178b6f17b9c3c8d12486d9d1e4deb
Amy Baldwin <abaldwin008@gmail.com>
2017-03-26 22:37:31 +0000

    Refactoring static pages and adding minitest

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Om Nom Crumbs"
+  end
+  
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Om Nom Crumbs"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Om Nom Crumbs"
+    assert_select "title", "About | #{@base_title}"
   end
   
   
